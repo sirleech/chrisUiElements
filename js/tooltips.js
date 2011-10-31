5c377ec96106f57240399381238bec9422673725
chris <sirleech@gmail.com>
2011-10-31 16:04:01 +1100

    fine tune tips

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,16 +1,22 @@
 $(document).ready(function()
 {
    $('button.tooltip-fancy[title]').qtip({
+	   position: {
+		  at: 'bottom center'
+   	   },
 	   style: {
 		  classes: 'ui-tooltip-dark ui-tooltip-shadow',
 		  tip: {
 			corner: true,
-			offset: 20
+			offset:0
 		  }
 	   }
 	});
 	
 	$('button.tooltip-help[title]').qtip({
+	   position: {
+		  at: 'bottom center'
+   	   },
 	   style: {
 		  classes: 'tooltip-help ui-tooltip-normal ui-tooltip-shadow ',
 		  tip: {
