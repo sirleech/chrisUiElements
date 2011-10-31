87c0b3815dd69ee5f26f9287936d9d66104aad5b
chris <sirleech@gmail.com>
2011-10-31 15:25:49 +1100

    help tooltip

--- js/tooltips.js
+++ js/tooltips.js
@@ -2,6 +2,17 @@ $(document).ready(function()
 {
    $('button.tooltip-fancy[title]').qtip({
 	   style: {
+		  classes: 'ui-tooltip-dark ui-tooltip-shadow',
+		  tip: {
+			corner: true,
+			offset: 20
+		  }
+	   }
+	});
+	
+	$('button.tooltip-help[title]').qtip({
+	   style: {
+		  classes: 'tooltip-help ui-tooltip-normal ui-tooltip-shadow ',
 		  tip: {
 			corner: true,
 			offset: 20
